bb30d1968a24c80613d5da14833af0262b9f8757
Collin Duncan <cgduncan7@gmail.com>
2020-04-13 22:36:29 +0200

    checking new route strategy

--- src/app.tsx
+++ src/app.tsx
@@ -5,44 +5,40 @@ import {
   Switch,
 } from 'react-router-dom';
 
-import Navigation, { INavLink } from './navigation';
-
-import Page from './page';
-import FourOhFour from './fourohfour';
-
-interface IRoute extends INavLink {
-  component: any,
-  exact?: boolean,
-}
-
 export default class App extends React.Component<{}, {}> {
-  private routes: IRoute[] = []
-
   constructor(props: any) {
     super(props);
-
-    this.routes = [
-      { path: '/', label: 'Hoofdpagina', component: <Page title="Hoofdpagina">Deze is de hoofdpagina</Page>, exact: true },
-      { path: '/taal-cafe', label: 'Taalcafé', component: <Page title="Taalcafé">Deze gaat over de taalcafé</Page> },
-      { path: '/korte-verhalen', label: 'Korte Verhalen', component: <Page title="Korte verhalen">Hier staan korte verhalen</Page> },
-      { path: '/debatclub', label: 'Debatclub', component: <Page title="Debatclub">D E B A T C L U B</Page> },
-    ]
   }
 
   render() {
     return (
       <Router>
-        <Navigation links={this.routes}/>
         <Switch>
-          {
-            this.routes.map(({ path, component, exact = false }, index) => (
-                <Route exact={exact} path={path} key={index}>
-                  { component }
-                </Route>
-            ))
-          }
-          <Route path='*' key='404'>
-            <FourOhFour title='404' />
+          <Route path="/en">
+            <Switch>
+              <Route exact path="/en">
+                Home page
+              </Route>
+              <Route path="/en/debate-club">
+                Debate club
+              </Route>
+              <Route path="*">
+                Page not found
+              </Route>
+            </Switch>
+          </Route>
+          <Route path="/">
+            <Switch>
+              <Route exact path="/">
+                Hoofdpagina
+              </Route>
+              <Route path="/debatclub">
+                Debatclub
+              </Route>
+              <Route path="*">
+                Pagina niet gevonden
+              </Route>
+            </Switch>
           </Route>
         </Switch>
       </Router>
