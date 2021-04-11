58b547898922692a0f6ce5bc704f407658bca5d6
cgduncan7 <cgduncan7@gmail.com>
2021-04-11 17:01:56 +0200

    Modified index.html for now and saved other as backup

--- src/app.tsx
+++ src/app.tsx
@@ -4,15 +4,41 @@ import {
   Route,
   Switch,
 } from 'react-router-dom';
+import Navigation, { INavLink } from './navigation';
 
 export default class App extends React.Component<{}, {}> {
+  private readonly links: INavLink[]
   constructor(props: any) {
     super(props);
+    this.links = [
+      {
+        label: 'Hoofdpagina',
+        path: '/',
+      },
+      {
+        label: 'Info',
+        path: '/info'
+      },
+      {
+        label: 'Taal Cafe',
+        path: '/taal-cafe'
+      },
+      {
+        label: 'Stukjes',
+        path: '/stukjes'
+      },
+      {
+        label: 'Debatclub',
+        path: '/debatclub'
+      },
+    ]
   }
 
   render() {
     return (
       <Router>
+        <Navigation links={this.links}>
+        </Navigation>
         <Switch>
           <Route path="/en">
             We gaan eerst Nederlands doen, balen.
@@ -21,6 +47,15 @@ export default class App extends React.Component<{}, {}> {
             <Switch>
               <Route exact path="/">
                 Hoofdpagina
+              </Route>
+              <Route path="/info">
+                Info
+              </Route>
+              <Route path="/taal-cafe">
+                Taal cafe
+              </Route>
+              <Route path="/stukjes">
+                Stukjes
               </Route>
               <Route path="/debatclub">
                 Debatclub
