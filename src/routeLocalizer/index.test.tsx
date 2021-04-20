3c15ad562d5a3b4ca5b5413e2f325e0070ed6e3e
cgduncan7 <cgduncan7@gmail.com>
2021-04-20 20:32:50 +0200

    Added some more tests

--- src/routeLocalizer/index.test.tsx
+++ src/routeLocalizer/index.test.tsx
@@ -6,26 +6,40 @@ import { ExtendedRouteConfig } from '../lib/routes'
 
 jest.mock('react-router-dom', () => ({ useLocation: () => ({ pathname: '/pieces' }) }))
 
-describe('routeLocalizer', () => {
-
-  const routes: ExtendedRouteConfig[] = [
-    {
-      component: () => <div>pieces</div>,
-      path: ['/stukjes', '/pieces'],
-      resolveLanguage: (path: string) => path === '/pieces' ? 'en' : 'nl',
-      resolvePath: (lang: string) => {
-        switch (lang.toLowerCase()) {
-          case 'en': return '/pieces'
-          case 'nl':
-          default: return '/stukjes'
-        }
-      },
-      titleKey: 'pieces',
-      navigable: true,
+const routes: ExtendedRouteConfig[] = [
+  {
+    component: () => <div>pieces</div>,
+    path: ['/stukjes', '/pieces'],
+    resolveLanguage: (path: string) => path === '/pieces' ? 'en' : 'nl',
+    resolvePath: (lang: string) => {
+      switch (lang.toLowerCase()) {
+        case 'en': return '/pieces'
+        case 'nl':
+        default: return '/stukjes'
+      }
     },
-  ]
+    titleKey: 'pieces',
+    navigable: true,
+  },
+]
 
-  it('should change route if resolved language does not match', () => {
+describe('routeLocalizer', () => {
+  it('should render children', () => {
+    const changeLanguage = () => {}
+    const wrapper = shallow(
+      <RouteLocalizer
+        routes={routes}
+        changeLanguage={changeLanguage}
+        language={'en'}
+      >
+        <div id="child">test</div>
+      </RouteLocalizer>
+    );
+    expect(wrapper).toBeDefined()
+    expect(wrapper.find('#child')).toBeDefined()
+  })
+
+  it('should change language if resolved language does not match', () => {
     const changeLanguage = jest.fn()
     const wrapper = shallow(
       <RouteLocalizer
@@ -38,6 +52,21 @@ describe('routeLocalizer', () => {
     );
     expect(changeLanguage).toHaveBeenCalledTimes(1)
     expect(changeLanguage).toHaveBeenCalledWith('en')
+    expect(wrapper).toBeDefined()
+  })
+
+  it('should not change language if resolved language does not match', () => {
+    const changeLanguage = jest.fn()
+    const wrapper = shallow(
+      <RouteLocalizer
+        routes={routes}
+        changeLanguage={changeLanguage}
+        language={'en'}
+      >
+        <div>test</div>
+      </RouteLocalizer>
+    );
+    expect(changeLanguage).toHaveBeenCalledTimes(0)
     expect(wrapper).toBeDefined()
   })
 })
