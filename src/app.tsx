039372c9bf5ce27c4e84f51d526af78b3e6567a7
cgduncan7 <cgduncan7@gmail.com>
2021-04-20 20:34:48 +0200

    using css grid now

--- src/app.tsx
+++ src/app.tsx
@@ -7,8 +7,83 @@ import { renderRoutes, RouteConfig } from 'react-router-config';
 import Navigation from './navigation';
 import Home from './home';
 
-export default class App extends React.Component<{}, {}> {
-  private readonly routes: RouteConfig[]
+import { ExtendedRouteConfig } from './lib/routes'
+
+const routes: ExtendedRouteConfig[] = [
+  {
+    component: Home,
+    exact: true,
+    path: '/',
+    resolvePath: () => '/',
+    titleKey: 'home',
+    navigable: true,
+  },
+  {
+    component: Pieces,
+    path: ['/stukjes', '/pieces'],
+    resolveLanguage: (path: string) => path === '/pieces' ? 'en' : 'nl',
+    resolvePath: (lang: string) => {
+      switch (lang.toLowerCase()) {
+        case 'en': return '/pieces'
+        case 'nl':
+        default: return '/stukjes'
+      }
+    },
+    titleKey: 'pieces',
+    navigable: true,
+  },
+  {
+    component: () => <div>Lessen</div>,
+    path: ['/les-nederlands', '/dutch-lessons'],
+    resolveLanguage: (path: string) => path === '/dutch-lessons' ? 'en' : 'nl',
+    resolvePath: (lang: string) => {
+      switch (lang.toLowerCase()) {
+        case 'en': return '/dutch-lessons'
+        case 'nl':
+        default: return '/les-nederlands'
+      }
+    },
+    titleKey: 'lessons',
+    navigable: true,
+  },
+  {
+    component: () => <div>Debatclub</div>,
+    path: ['/debatclub', '/debate-club'],
+    resolveLanguage: (path: string) => path === '/debate-club' ? 'en' : 'nl',
+    resolvePath: (lang: string) => {
+      switch (lang.toLowerCase()) {
+        case 'en': return '/debate-club'
+        case 'nl':
+        default: return '/debatclub'
+      }
+    },
+    titleKey: 'debate_club',
+    navigable: true,
+  },
+  {
+    component: () => <div>Taal cafe</div>,
+    path: ['/taal-cafe', '/language-cafe'],
+    resolveLanguage: (path: string) => path === '/language-cafe' ? 'en' : 'nl',
+    resolvePath: (lang: string) => {
+      switch (lang.toLowerCase()) {
+        case 'en': return '/language-cafe'
+        case 'nl':
+        default: return '/taal-cafe'
+      }
+    },
+    titleKey: 'language_cafe',
+    navigable: true,
+  },
+  {
+    component: () => <FourOhFour />,
+    path: '*',
+    titleKey: '404',
+    navigable: false,
+  }
+]
+
+class App extends React.Component<WithTranslation, {}> {
+  private readonly routes: ExtendedRouteConfig[]
   constructor(props: any) {
     super(props);
     this.routes = [
