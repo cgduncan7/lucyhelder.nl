fc1e10104f2cc38ee1582cbb7d51fd0869b20ef8
Collin Duncan <cgduncan7@gmail.com>
2022-07-03 11:10:04 +0200

    Allowing CORS requests from lucyhelder.nl

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,16 @@
 import express from 'express'
+import cors from 'cors'
 import { router as registrationRouter } from './registration'
 
 const app = express()
 
+const corsOptions = {
+  origin: 'https://lucyhelder.nl'
+}
+
 app.disable('x-powered-by')
 app.use(express.json({ limit: '1mb' }))
+app.use(cors(corsOptions));
 
 app.use('/registration', registrationRouter)
 
